e43a40f9825d7c47e5d650159cee52c211810cdb
Afzal ziqri <19104002@ittelkom-pwt.ac.id>
2020-10-11 00:28:44 +0700

    master

---
+++ src/com/afzal/tugas1/Karyawan/No3.java
@@ -0,0 +1,17 @@
+package com.afzal.tugas1.Karyawan;
+
+public class No3 {
+    // Deklarasi
+    private static String nama="Afzal";
+    private static String asal="Makassar";
+    private static String umur="19";
+    private static String jk="Laki-laki";
+
+    public static void main(String[] args) {
+        // Output
+        System.out.println("Nama\t\t\t\t: "+nama);
+        System.out.println("Asal\t\t\t\t: "+asal);
+        System.out.println("Umur\t\t\t\t: "+umur);
+        System.out.println("Jenis Kelamin\t\t: "+jk);
+    }
+}
